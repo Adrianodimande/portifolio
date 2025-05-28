712502e93a4b9c5fe4480f11e939ddaaaed33e00
Adrianodimande <adrianovelemo76@gmail.com>
2025-05-28 21:14:11 +0200

    2 versao

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,11 @@
-module.exports = {
-  darkMode:"class",
-  content: ['./*.html', './*.js', './src/**/*.{html,js}'],
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {
-      spacing:{
-        "big":"48rem"
-      }
-    },
-    screens:{
-      sm:"480px",
-      md:"768px",
-      lg:"1024px",
-      xl:"1280px",
-    },
-    fontFamily:{
-      nunito:['Nunito', 'sans-serif']
-    }
+    extend: {},
   },
   plugins: [],
 }
